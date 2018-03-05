820484124c7df1457ce418cb44c0355159522ef6
Yuka Kanegae <g1444431@cc.kyoto-su.ac.jp>
2018-03-05 18:56:48 +0900

    20180304ABCのA問題、B問題

---
+++ 20180304/b.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class b{
+    public static void main(String[] args){
+			Scanner sc = new Scanner(System.in);
+			int N = sc.nextInt();
+			sc.nextLine();
+			String S = sc.nextLine();
+			String[] arare = S.split(" ", 0);
+			List<String> listA = new ArrayList<String>();
+			for(int i=0; i<arare.length; i++){
+				listA.add(arare[i]);
+				//System.out.println(S);
+			}
+			//System.out.println(listA);
+			List<String> listB = new ArrayList<String>(new HashSet<>(listA));
+			//System.out.println(listB.size());
+			if(listB.size()==3){
+				System.out.println("Three");
+			}else if(listB.size()==4){
+				System.out.println("Four");
+			}
+		}
+}
