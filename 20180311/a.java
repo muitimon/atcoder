6353f98b1c120f41d57696d7c728e59d5343041d
Yuka Kanegae <g1444431@cc.kyoto-su.ac.jp>
2018-03-12 17:37:19 +0900

    ABC090 A問題

---
+++ 20180311/a.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+			Scanner sc = new Scanner(System.in);
+			//String tmp = sc.nextLine();
+			System.out.print(sc.nextLine().substring(0,1));
+			System.out.print(sc.nextLine().substring(1,2));
+			System.out.print(sc.nextLine().substring(2,3));
+    }
+}
