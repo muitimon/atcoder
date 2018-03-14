04d759ce969d7ac751e7f3616f75e493b777b311
Yuka Kanegae <g1444431@cc.kyoto-su.ac.jp>
2018-03-14 22:23:30 +0900

    authorized_keyが設定されているのか確認のためのテスト

---
+++ 20180218/c.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class c{
+    public static void main(String[] args){
+			Scanner sc = new Scanner(System.in);
+			int c[][] = new int[3][3];
+			for(int i=0; i<3; i++){
+				for(int j=0; j<3; j++){
+					c[i][j] = sc.nextInt();
+				}
+			}
+			int a[] = new int[3];
+			int b[] = new int[3];
+			if(c[0][0]
+		}
+}
